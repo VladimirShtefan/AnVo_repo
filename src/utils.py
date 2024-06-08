381f951b4467e688ca0f99e294e55bac0c84e716
annavaleriev <filenko.a@gmail.com>
2024-06-08 18:25:29 +0300

    исправить комментарии

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,6 @@
 def get_sum(x: int, y: int) -> float:
     """
     Функция, которая реализует сложение
-    :param x: число
-    :param y: число
-    :return: сумма чисел
     """
     result = x + y
     return result
